d0e88b7c8e1a60aa283270ca3c8e555394a3d061
Scott Schupbach <sschupbach@asperasoft.com>
2016-10-17 17:21:05 -0700

    Refactorings

--- app.js
+++ app.js
@@ -4,18 +4,58 @@ Array.prototype.sample = function () {
 
 var app = angular.module('toy-app', []);
 
-app.controller('BodyCtrl', $scope => {
-  $scope.colors = ['red', 'blue', 'purple', 'black', 'white'];
+app.controller('BodyCtrl', ($interval, $scope) => {
+  $scope.colors = [
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'purple',
+    'brown'
+  ];
+
+  $scope.smoothChange = (i, j) => {
+    $scope.bgColor = $scope.colors[i];
+    $scope.color = $scope.colors[j];
+  };
+
+  $scope.randomColors = () => {
+    var c1 = $scope.colors.sample(),
+        c2 = $scope.colors.sample();
+    while (c1 === c2) {
+      c2 = $scope.colors.sample();
+    }
+    $scope.bgColor = c1;
+    $scope.color = c2;
+  };
+
+  $scope.toggleAutoChange = () => {
+    if ($scope.interval) {
+      $interval.cancel($scope.interval);
+      $scope.interval = null;
+    } else {
+      var i = $scope.colors.length - 1,
+          j = Math.floor($scope.colors.length / 2);
+      $scope.smoothChange(i, j);
+      $scope.interval = $interval(() => {
+        i = (i + 1) % $scope.colors.length;
+        j = (j + 1) % $scope.colors.length;
+        $scope.smoothChange(i, j);
+      }, 2000)
+    }
+  };
+
+  $scope.isAutoChanging = () => {
+    return $scope.interval;
+  };
+
   $scope.$on('Change:background', (_ev, color) => {
-    $scope.bgColor = 'on-' + color;
+    $scope.bgColor = color;
   });
   $scope.$on('Change:text', (_ev, color) => {
     $scope.color = color;
   });
-  $scope.randomColors = () => {
-    $scope.bgColor = 'on-' + $scope.colors.sample();
-    $scope.color = $scope.colors.sample();
-  }
 });
 
 app.controller('ToyButtonCtrl', ($scope, $rootScope) => {
@@ -31,10 +71,4 @@ app.component('toyButton', {
     color: '=',
     colorType: '='
   }
-});
-
-app.filter('pretty', function () {
-  return function (word) {
-    return word.split('-').join(' ');
-  };
 });
